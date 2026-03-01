996b6a49a20fcbd6f82bb5c81429b21dfaccf8e6
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-03-01 14:26:55 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
 	defaultConfig {
 		applicationId = "dev.rushii.xspoofsignatures"
-		minSdk = 3 // Yes, this is accurate
+		minSdk = 30
 		targetSdk = 35
 		versionCode = 2
 		versionName = "1.0.1-noroot"
